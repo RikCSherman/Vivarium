f5eabc28537e6760108564b5bd7600837db1abc9
Rik Sherman <rik@example.com>
2022-04-19 11:15:06 +0100

    Fix buffer sizing passed to snprintf

--- lib/utils/src/utils.cpp
+++ lib/utils/src/utils.cpp
@@ -3,7 +3,7 @@
 String formatTime(tm timeinfo) {
     char buffer[21];
     int sizeOfString =
-        snprintf(buffer, 100, "%4d-%02d-%02dT%02d:%02d:%02dZ",
+        snprintf(buffer, 21, "%4d-%02d-%02dT%02d:%02d:%02dZ",
                  1900 + timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday,
                  timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
     if (sizeOfString <= 0 || sizeOfString > 21) {
