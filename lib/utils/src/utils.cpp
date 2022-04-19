4ee482220109165370b224cadfee25adbeef69e9
Rik Sherman <rik@example.com>
2022-04-19 11:16:07 +0100

    extract buffer size to const

--- lib/utils/src/utils.cpp
+++ lib/utils/src/utils.cpp
@@ -1,12 +1,13 @@
 #include "utils.h"
 
 String formatTime(tm timeinfo) {
-    char buffer[21];
+    const int dateStringSize = 21;
+    char buffer[dateStringSize];
     int sizeOfString =
-        snprintf(buffer, 21, "%4d-%02d-%02dT%02d:%02d:%02dZ",
+        snprintf(buffer, dateStringSize, "%4d-%02d-%02dT%02d:%02d:%02dZ",
                  1900 + timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday,
                  timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-    if (sizeOfString <= 0 || sizeOfString > 21) {
+    if (sizeOfString <= 0 || sizeOfString > dateStringSize) {
         Serial.println("Oops problem with time printing");
         return "1970-00-01T00:00:01Z";
     }
