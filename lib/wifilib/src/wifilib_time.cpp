580539a26b01605303bbab3da35d64bc594c53d6
Rik Sherman <rik@example.com>
2022-05-01 09:19:27 +0100

    Check for valid time

--- lib/wifilib/src/wifilib_time.cpp
+++ lib/wifilib/src/wifilib_time.cpp
@@ -4,8 +4,11 @@
 
 void initialiseTime() { configTime(0, 0, "pool.ntp.org"); }
 
+const uint32_t dhtReadingTickDelay = pdMS_TO_TICKS(10);
 String getTime() {
     struct tm timeinfo;
-    getLocalTime(&timeinfo);
+    while (!getLocalTime(&timeinfo, 50)) {
+        vTaskDelay(dhtReadingTickDelay);
+    }
     return formatTime(timeinfo);
 }
